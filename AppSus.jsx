728d1639f06f501c889a10d46ef25e50d32e5863
Your Name <you@example.com>
2021-05-04 13:50:52 +0300

    start work on main app

--- AppSus.jsx
+++ AppSus.jsx
@@ -1,12 +1,13 @@
-const Router = ReactRouterDOM.HashRouter
-const { Route, Switch } = ReactRouterDOM
+import {UserPreview} from './cmps/user-preview.jsx'
 import {Home} from './pages/home.jsx'
 
 // Simple React Component
 export function App() {
     return <section className="app">
        <h1>My App</h1>
+       <UserPreview/>
        <Home/>
     </section>
 }
+
 
