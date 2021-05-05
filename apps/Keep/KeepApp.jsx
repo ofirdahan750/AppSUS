e6c451590d6e72d40f76d25d16ed4871c452be15
Jimmy <jimbok1212@gmail.com>
2021-05-05 16:18:45 +0300

    geting notes array

--- apps/Keep/KeepApp.jsx
+++ apps/Keep/KeepApp.jsx
@@ -1,8 +1,11 @@
 import { keepService } from './services/keep-service.js'
+// import { NotePreview } from './cmps/NotePreview.jsx'
 
 export class KeepApp extends React.Component {
     state = {
-        notes: []
+        notes: null,
+        note: null,
+
     }
     componentDidMount() {
         this.loadNotes()
@@ -10,15 +13,23 @@ export class KeepApp extends React.Component {
     loadNotes = () => {
         let notes = keepService.notesQuery()
         notes.then(notes => {
-            this.setState({ notes } , () => {
-                console.log(this.state);
-            }) 
+            this.setState({ notes })
+            // console.log(this.state.notes)
         })
     }
+
     render() {
+        const { notes } = this.state
+        console.log(notes);
+
         return (
-            <section>
-                <h1>hello</h1>
+            <section className="note-preview" >
+                { notes && 
+                notes.map(note => {
+                    <h1>
+                        {this.state.notes}
+                    </h1>
+                })}
             </section>
         )
     }
---
+++ apps/Keep/cmps/NotePreview.jsx
@@ -0,0 +1,11 @@
+// const { Link } = ReactRouterDOM
+// export function NotePreview({ note }) {
+//   const previewClass = note
+//   return (
+    
+//       <article className={'note-preview ' + previewClass}>
+//         <p>note - {note}</p>
+//         <Link to={`/keep`}>KeepApp</Link>
+//       </article>
+//   )
+// }
