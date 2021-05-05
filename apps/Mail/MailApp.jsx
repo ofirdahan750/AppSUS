6e22722b467c8c82ea9bd3befe589a94efc969c7
Your Name <you@example.com>
2021-05-05 13:58:32 +0300

    css basic fixed + start working on mail app

---
+++ apps/Mail/MailApp.jsx
@@ -0,0 +1,11 @@
+
+
+export class MailApp extends React.Component {
+    render() {
+        return (
+            <section>
+                <h1>maillll</h1>
+            </section>
+        )
+    }
+}
