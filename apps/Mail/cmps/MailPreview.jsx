ea2e93203161fb9b6383ba7462e90184d89f4aa8
Your Name <you@example.com>
2021-05-08 10:46:19 +0300

    fixing the toggle class 100%

--- apps/Mail/cmps/MailPreview.jsx
+++ apps/Mail/cmps/MailPreview.jsx
@@ -1,20 +1,23 @@
-import { EmailService } from '../../Mail/services/mail-service.js'
-import { eventBusService } from '../../../services/event-bus-service.js'
+// import { EmailService } from '../../Mail/services/mail-service.js'
+// import { eventBusService } from '../../../services/event-bus-service.js'
 export class MailPerview extends React.Component {
 
-toggleRead() {
-    const { email } = this.props
-    this.props.onToggleRead(email)
-}
+    toggleRead(clickSource) {
+        const { email } = this.props
+        if (clickSource === 'open-mail') console.log('need to go mail')
+            if (clickSource === 'button-mail' || clickSource === 'open-mail' && !email.isRead) this.props.onToggleRead(email)
+
+    }
 
 
-render() {
-    const { email } = this.props
-    if (email) return (
-        <div className={`email-preview ` + email.isRead ? '' : 'is-read'} onClick={() => this.toggleRead(email)}>
-            { email.from} || { `${email.title.substring(0, 20)}..`} || { email.hour}:{email.min}
-        </div >
-    )
-    else return <div>loading...</div>
-}
+    render() {
+        const { email } = this.props
+        if (email) return (
+            <div className={`email-preview ${email.isRead ? 'is-unread' : 'is-read'}`} onClick={() => this.toggleRead('open-mail')}>
+                { email.from} || { `${email.title.substring(0, 20)}..`} || { email.hour}:{email.min}
+                <button onClick={() => this.toggleRead('button-mail')} className={`email-preview ${email.isRead ? 'far fa-envelope-open' : 'fas fa-envelope-open'}`}></button>
+            </div >
+        )
+        else return <div>loading...</div>
+    }
 }
