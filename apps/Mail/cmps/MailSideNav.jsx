ea8aa150619131f95b777dcb90609644bfd82888
Your Name <you@example.com>
2021-05-08 15:33:56 +0300

    git link

--- apps/Mail/cmps/MailSideNav.jsx
+++ apps/Mail/cmps/MailSideNav.jsx
@@ -2,10 +2,11 @@ const { Link } = ReactRouterDOM
 export class MailSideNav extends React.Component {
     render() {
         return (
-            <section class="mail-sidenav">
-               <Link> <button>+Compose</button></Link>
-                <button>Inbox</button>
-                <button>Send</button>
+            <section className="email-sidenav flex column">
+               <Link to="/mail/Compose"><button>+Compose</button></Link>
+               <Link to="/mail/Inbox"><button>Inbox</button></Link>
+               <Link to="/mail/Send"><button>Send</button></Link>
+
             </section>
         )
     }
